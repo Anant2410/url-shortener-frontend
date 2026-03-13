09df8e545776ee899e8fd3e9aa3dc9b6c9c894cd
Anant <anantgarg2410@gmail.com>
2026-03-13 22:11:06 +0530

    tailwind configure and about page work

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+let desc =
+  "Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms. Optimize your sharing strategy with Linklytics. Track clicks and manage your links seamlessly to enhance your online presence. Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms.";
+
+
+
+const LandingPage = () => {
+  return (
+    <div>LandingPage</div>
+  )
+}
+
+export default LandingPage
