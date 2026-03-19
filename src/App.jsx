788c700dc5b507e5fcaa178a3d13ffdc4573e253
Anant <anantgarg2410@gmail.com>
2026-03-19 23:45:09 +0530

    login page and jwt setup from backend

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import RegisterPage from "./components/RegisterPage";
 import { Toaster } from "react-hot-toast";
+import LoginPage from "./components/LoginPage";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
       <Footer />
     </Router>
