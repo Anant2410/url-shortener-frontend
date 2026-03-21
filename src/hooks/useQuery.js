f032dffe849c6495b4f424733782f768f8e14cf2
Anant <anantgarg2410@gmail.com>
2026-03-21 16:33:37 +0530

    fetching the data from the backend reagrding total clicks

---
+++ src/hooks/useQuery.js
@@ -0,0 +1,56 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../api/api";
+
+// export const useFetchMyShortUrls = (token, onError) => {
+//   return useQuery({
+//     queryKey: ["my-shortenurls", token],
+//     queryFn: async () => {
+//       const response = await api.get("/api/urls/myurls", {
+//         headers: {
+//           "Content-Type": "application/json",
+//           Accept: "application/json",
+//           Authorization: `Bearer ${token}`,
+//         },
+//       });
+//       return response.data;
+//     },
+//     enabled: !!token,
+//     select: (data) => {
+//       return [...data].sort(
+//         (a, b) => new Date(b.createdDate) - new Date(a.createdDate)
+//       );
+//     },
+//     onError,
+//     staleTime: 5000,
+//   });
+// };
+
+export const useFetchTotalClicks = (token, onError) => {
+  return useQuery({
+    queryKey: ["url-totalclick", token],
+    queryFn: async () => {
+      const response = await api.get(
+        "/api/urls/totalClicks?startDate=2026-03-05&endDate=2026-03-16",
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    },
+    enabled: !!token,
+    select: (data) => {
+      return Object.keys(data).map((key) => ({
+        clickDate: key,
+        count: data[key],
+      }));
+    },
+    onError,
+    staleTime: 5000,
+  });
+};
+
+
